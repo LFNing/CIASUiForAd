1bcf306226c1ccf77ed24d355e99209db03200fb
LEEEFN <42526807+LFNing@users.noreply.github.com>
2019-08-27 21:15:32 +0800

    Update DemoServelt.java

--- src/util/DemoServelt.java
+++ src/util/DemoServelt.java
@@ -8,31 +8,61 @@ import java.io.PrintWriter;
 
 //接收客户端的参数并响应客户端
 public class DemoServelt extends HttpServlet {
-    private static  final long servialVersionUID = 1L;
+//    private static  final long servialVersionUID = 1L;
+//
+//    public void doGet(HttpServletRequest request, HttpServletResponse response)
+//            throws  ServletException, IOException{
+//        request.setCharacterEncoding("utf-8");
+//        response.setContentType("text/html;charset=utf-8");
+//        String id = request.getParameter("id");
+//        String name = request.getParameter("name");
+//        String password = request.getParameter("password");
+//        PrintWriter pw = response.getWriter();
+//        pw.print("使用GET方式请求该Servlet"+"id = "+id+",name = "+name+",password = "+password);
+//        pw.flush();
+//        pw.close();
+//    }
+//
+//    public void doPost(HttpServletRequest request,HttpServletResponse response)
+//        throws ServletException,IOException{
+//        request.setCharacterEncoding("utf-8");
+//        response.setContentType("text/html;charset=utf-8");
+//        String id = request.getParameter("id");
+//        String name = request.getParameter("name");
+//        String password = request.getParameter("password");
+//        PrintWriter pw = response.getWriter();
+//        pw.print("使用POST方式请求servlet"+"id = "+id+"name = "+name+",password = "+password);
+//        pw.flush();
+//        pw.close();
+//    }
 
-    public void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws  ServletException, IOException{
-        request.setCharacterEncoding("utf-8");
-        response.setContentType("text/html;charset=utf-8");
-        String id = request.getParameter("id");
-        String name = request.getParameter("name");
-        String password = request.getParameter("password");
-        PrintWriter pw = response.getWriter();
-        pw.print("使用GET方式请求该Servlet"+"id = "+id+",name = "+name+",password = "+password);
-        pw.flush();
-        pw.close();
-    }
+    SqlConnect sqlConnect = new SqlConnect();
 
-    public void doPost(HttpServletRequest request,HttpServletResponse response)
-        throws ServletException,IOException{
-        request.setCharacterEncoding("utf-8");
+    protected void doPost(HttpServletRequest request,HttpServletResponse response)
+            throws ServletException,IOException{
         response.setContentType("text/html;charset=utf-8");
-        String id = request.getParameter("id");
-        String name = request.getParameter("name");
-        String password = request.getParameter("password");
-        PrintWriter pw = response.getWriter();
-        pw.print("使用POST方式请求servlet"+"id = "+id+"name = "+name+",password = "+password);
-        pw.flush();
-        pw.close();
+        request.setCharacterEncoding("utf-8");
+        response.setCharacterEncoding("utf-8");
+        PrintWriter out = response.getWriter();
+
+        String id;
+        String name;
+        String password;
+
+        id = request.getParameter("id");
+        name = request.getParameter("name");
+        password = request.getParameter("password");
+
+        if(name.equals("xingming")&&password.equals("mima")){
+            out.print("Login succeeded!");
+            out.println();
+            out.print("name:"+name+";password"+password);
+        }else{
+            out.print("Login failed!");
+            out.println();
+        }
+
+        out.flush();
+        out.close();
     }
 }
